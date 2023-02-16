eed3aaaecd77b532632f50b708cafe58a0a2cdca
shahzaibahmad80 <shahzaib.ahmad6480@gmail.com>
2023-02-16 15:11:23 +0500

    4th commit

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,3 +1,14 @@
+import math
+
+# Define the two points
+point1 = (2, 3)
+point2 = (10, 8)
+
+# Calculate the Euclidean distance
+distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
+# Print the result
+print(distance)
 
 print (3+4)
 print (3-4)
@@ -35,6 +46,6 @@ d= { 'city1':'lahore',
 }
 f= set()
 print(type(f))
-
+print (l)
 
 
