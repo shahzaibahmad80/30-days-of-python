3bf4ef26e5e4be65785f7caecc0168e9c407ab84
shahzaibahmad80 <shahzaib.ahmad6480@gmail.com>
2023-02-19 23:03:29 +0500

    first commit

---
+++ day_3/day_3.py
@@ -0,0 +1,51 @@
+age = 21
+height = 5.4
+c = 1 + 2j
+
+base = int (input("Enter the base:  ")) 
+height = int (input("Enter the height:  "))
+area = 0.5 * base *height
+print("the area of triangle is: ",  area)
+
+a = float (input("Enter side a: ")) 
+b = float (input("Enter side b: ")) 
+c = float (input("Enter side c: ")) 
+perimeter = a+b+c
+print("the perimeter of the triangle is:  ", perimeter)
+
+length  = int (input("Enter the length:  ")) 
+width = int (input("Enter the width:  "))
+area1 = length * width
+perimeter1 = 2 * (length * width)
+print("the area1 of triangle is: " , area1)
+print("the perimeter1 of triangle is: " , perimeter1)
+
+radius = float(input("Enter the raidius of a circle: "))
+pi = 3.1415
+area2 =  pi * radius * radius
+circumference = 2 * pi * radius
+print("Area2 of circle is: " , area2)
+print("circumference of circle is: " , circumference)
+
+
+equation = "y = 2x - 2"
+m = 2
+b = -2
+x_intercept = -b/m
+print("Slope: ", m)
+print("Y-intercept: ", b)
+print("X-intercept: (", x_intercept, ", 0)")
+
+import math
+x1, y1 = 2, 2
+x2, y2 = 6, 10
+m = (y2 - y1) / (x2 - x1)
+d = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print("Slope: ", m)
+print("Euclidean distance: ", d)
+
+x = float (input("Enter the value of x:  "))
+y = x**2 +6x + 9
+print("The value of y is : ", y )
+
+
